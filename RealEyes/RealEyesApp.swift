f4f2e44a58bd4a502451784a649d6caed5493365
Faustin Veyssiere <faustin.veyssiere@gmail.com>
2025-07-04 10:19:05 +0200

    Initial commit

---
+++ RealEyes/RealEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEyesApp.swift
+//  RealEyes
+//
+//  Created by Ptitin on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RealEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
